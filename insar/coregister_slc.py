389a85f50c7bffedbf207878ec49c4a7c377b550
Mitchell Wheeler <mitchell.wheeler@gmail.com>
2021-03-22 14:55:34 +1100

    Added thread-local context to SLC coreg and IFG processing, and also now logging stdout for all gamma commands even on success - this allows us to produce traces of specific SLC and IFG tasks and see their output in case of unexpected issues that aren't gamma failures

--- insar/coregister_slc.py
+++ insar/coregister_slc.py
@@ -1564,6 +1564,17 @@ class CoregisterSlc:
 
     def main(self):
         """Main method to execute methods sequence of methods need for master-slave coregistration."""
+
+        # Re-bind thread local context to IFG processing state
+        structlog.contextvars.clear_contextvars()
+        master_date, slave_date = ic.ifg_dir.name.split('-')
+        structlog.contextvars.bind_contextvars(
+            task="SLC coregistration",
+            slc_dir=self.out_dir,
+            master_date=self.master_date,
+            slave_date=self.slave_date
+        )
+
         with working_directory(self.out_dir):
             self.set_tab_files()
             self.get_lookup()

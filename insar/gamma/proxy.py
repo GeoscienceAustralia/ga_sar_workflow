4be5ea75fc2bb3f25f7598de5f1f659256fc9395
debs21 <78889117+debs21@users.noreply.github.com>
2023-08-28 16:21:08 +1000

    Change PyGamma to ga_sar_workflow, PyGammaProxy to GammaProxy

--- insar/gamma/proxy.py
+++ insar/gamma/proxy.py
@@ -10,7 +10,7 @@ from insar.gamma.generated import PyGammaProxy
 
 def get_gamma_version() -> str:
     """
-    Get the version of GAMMA being used by the `PyGamma` workflow.
+    Get the version of GAMMA being used by the `ga_sar_workflow` workflow.
 
     This is simply a wrapper around getting the `GAMMA_VER` env var currently, which is how
     `gammar_insar` is designed to work with it's virtual environment / configuration setup scripts.
@@ -28,15 +28,15 @@ def create_versioned_gamma_proxy(gamma_ver: str, base_wrapper: object, exception
     Creates a GAMMA proxy object for a specific version of GAMMA, which translates from the API for
     GAMMA version 20191203 into the user specified GAMMA version.
 
-    This is used so `PyGamma` can be written for one specific GAMMA version without having to
+    This is used so `ga_sar_workflow` can be written for one specific GAMMA version without having to
     be re-written or constantly re-ported to newer versions, by instead using these translation
     layers / proxy objects.
     """
     wrapper_module = importlib.import_module(f"insar.gamma.versions.v{gamma_ver}")
-    return cast(PyGammaProxy, wrapper_module.PyGammaProxy(exception_type, base_wrapper))
+    return cast(GammaProxy, wrapper_module.GammaProxy(exception_type, base_wrapper))
 
 
-def create_gamma_proxy(exception_type: Type[Exception]) -> PyGammaProxy:
+def create_gamma_proxy(exception_type: Type[Exception]) -> GammaProxy:
     """
     A convenience function for `create_versioned_gamma_proxy` that creates a proxy object for the
     currently installed version of GAMMA that wraps the `insar.py_gamma_ga` GAMMA call dispatch interface.
